58b576cd6811b5da7dcd457e5329ed507b975a3f
alasacelui <alasacelui010@gmail.com>
2021-09-09 18:43:05 +0800

    added Readme.MD

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Practice Spatie Media Library</title>
+    <title> Image Upload </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
